4aacd498e8117ed25cb5ed68fb3dcba65b481da4
Racero21 <rjacero@up.edu.ph>
2024-04-23 13:53:58 +0800

    feat: added AppBar

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import axios from 'axios';
 import Logger from './Logger';
 import Chart from './Chart';
 import ToggleButtonsMultiple from './Toggle';
-import { render } from '@testing-library/react';
 
 function App() {
     const [data, setData] = useState([]);
-    const [view, setView] = useState("Chart");
+    const [error, setError] = useState(null);
     const [selectedComponent, setSelectedComponent] = useState('statistics');
-    // const [loggers, setLoggers] = useState([]);
 
     const handleToggleChange = (event, newComponent) => {
         if (newComponent !== null)
@@ -19,7 +17,10 @@ function App() {
     const renderComponent = () => {
         switch (selectedComponent) {
           case 'statistics':
-            return <Chart />;
+            console.log(data)
+            return data.map(item => (
+                <div key={item.LoggerId}> <Chart id={item.LoggerId}/></div>  
+            ))
           case 'map':
             return <div><h1> MAP HERE </h1></div>;
           case 'config':
@@ -27,48 +28,30 @@ function App() {
           case 'other':
             return <Logger />;
           default:
-            return <Chart />;
+            return <h1>XDD</h1>;
         }
       };
 
     useEffect(() => {
       // Make an API call to your Node.js backend using axios
-      axios.get('http://192.168.3.18,9:3001/api/flow_logger')
-          .then(response => {
-              // Set the data in state
-              setData(response.data);
-              console.log("Success!!");
-              console.log(data);
-          })
-          .catch(error => {
-              console.error('Error fetching data:', error);
-          });  
+      const fetchUniqueLoggers = async () => {
+            try {
+                const response = await axios.get('http://192.168.3.189:3001/api/logger/');
+                const data = response.data;
+                
+                setData(data)
+            } catch (error) {
+                setError('Error fetching data: ' + error.message);
+            }
+        }
       
-    //   axios.get('http://192.168.3.179:3001/api/logger')
-    //       .then(response => {
-    //           // Set the data in state
-    //           setLoggers(response.data);
-    //           console.log("Success!!");
-    //           console.log(loggers);
-    //       })
-    //       .catch(error => {
-    //           console.error('Error fetching data:', error);
-    //       });  
+      fetchUniqueLoggers()
+
   }, [selectedComponent]);
   
     return (
         <div>
-
-            {/* <h1>Data from flow_logger</h1>
-            <ul>
-                {data.map(item => (
-                    <li key={item.LogId}>
-                        {item.LogId}: {item.LoggerModel} - {item.AverageVoltage}V
-                    </li>
-                ))}
-            </ul>
-            <Logger />
-            <Chart /> */}
+            {/* <AppBar /> */}
             <ToggleButtonsMultiple onChange={handleToggleChange}/>
             {renderComponent()}
         </div>
@@ -76,34 +59,3 @@ function App() {
 }
 
 export default App;
-
-// function App() {
-//     const [data, setData] = useState([]);
-
-//     useEffect(() => {
-//       // Make an API call to your Node.js backend using axios
-//       axios.get('http://192.168.3.179:3001/api/flow_logger')
-//           .then(response => {
-//               // Set the data in state
-//               setData(response.data);
-//               console.log("Success!!");
-//               console.log(data);
-//           })
-//           .catch(error => {
-//               console.error('Error fetching data:', error);
-//           });  
-//   }, []);
-  
-//     return (
-//         <div>
-//             <h1>Data from flow_logger</h1>
-//             <ul>
-//                 {data.map(item => (
-//                     <li key={item.LogId}>
-//                         {item.LogId}: {item.LoggerModel} - {item.AverageVoltage}V
-//                     </li>
-//                 ))}
-//             </ul>
-//         </div>
-//     );
-// }
--- src/Toggle.js
+++ src/Toggle.js
@@ -1,47 +1,48 @@
-import * as React from 'react';
+import React from 'react';
 import InsightsIcon from '@mui/icons-material/Insights';
 import MapIcon from '@mui/icons-material/Map';
 import TuneIcon from '@mui/icons-material/Tune';
-import DehazeIcon from '@mui/icons-material/Dehaze';
-import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
-import ToggleButton from '@mui/material/ToggleButton';
-import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Draggable from 'react-draggable';
-
+import Button from '@mui/material/Button';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import SettingsIcon from '@mui/icons-material/Settings';
+import { AppBar } from '@mui/material';
 
 export default function ToggleButtonsMultiple({ onChange }) {
-//   const [formats, setFormats] = React.useState(() => ['bold', 'italic']);
-
-//   const handleFormat = (event, newFormats) => {
-//     setFormats(newFormats);
-//   };
-
   return (
-    <Draggable>
-        <ToggleButtonGroup
-            exclusive
-            onChange={onChange}
-            aria-label="select component"
-        >
-        <ToggleButton value="statistics" aria-label="statistics">
-            <InsightsIcon />
-        </ToggleButton>
-        <ToggleButton value="map" aria-label="map">
-            <MapIcon />
-        </ToggleButton>
-        <ToggleButton value="config" aria-label="config">
-            <TuneIcon />
-        </ToggleButton>
-        <ToggleButton value="other" aria-label="other" disabled>
-            <DehazeIcon />
-            <ArrowDropDownIcon />
-        </ToggleButton>
-        </ToggleButtonGroup>
-    </Draggable>
-    
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+          <Button color="inherit" onClick={() => onChange(null, 'statistics')}>
+            <InsightsIcon sx={{ mr: 1 }} />
+            Dashboard
+          </Button>
+          <Button color="inherit" onClick={() => onChange(null, 'map')}>
+            <MapIcon sx={{ mr: 1 }} />
+            Map
+          </Button>
+          <Button color="inherit" onClick={() => onChange(null, 'config')}>
+            <TuneIcon sx={{ mr: 1 }} />
+            Config
+          </Button>
+          </Typography>
+          <Button color="inherit" onClick={() => onChange(null, 'settings')}><SettingsIcon />Settings</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
   );
 }
