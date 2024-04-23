51258f1a85afef715858698cc220261c65952742
Racero21 <rjacero@up.edu.ph>
2024-04-23 13:58:46 +0800

    UI: prepared config page for logger details

--- src/Logger.js
+++ src/Logger.js
@@ -6,7 +6,7 @@ function Logger() {
 
     useEffect(() => {
       // Make an API call to your Node.js backend using axios
-      axios.get('http://192.168.3.179:3001/api/logger')
+      axios.get('http://192.168.3.189:3001/api/logger')
           .then(response => {
               // Set the data in state
               setLoggers(response.data);
@@ -16,15 +16,18 @@ function Logger() {
           .catch(error => {
               console.error('Error fetching data:', error);
           });  
-  }, []);
+  }, [loggers]);
   
     return (
         <div>
             <h2>Distinct Flow Loggers</h2>
             {loggers.map((item, index) => (
-                    <li key={index}>
-                        {item.LoggerId}
-                    </li>
+                    // <li key={index}>
+                    <pre>
+                        LoggerId: {item.LoggerId} Model: {item.LoggerModel} <button>edit</button>
+                    </pre>
+                        
+                    // </li>
                 ))}
             {/* <ol>
                 {loggers.map((index, logger) => (
